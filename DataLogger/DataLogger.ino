e5eb5a89218e9e685e41367bfdc233718264a5e4
Khalil3Amer <khalil.3amer2000@gmail.com>
2022-05-21 12:26:17 +0300

    basic data retrival and send thru BLE with ACK

--- DataLogger/DataLogger.ino
+++ DataLogger/DataLogger.ino
@@ -4,7 +4,7 @@
 #include "DHT.h"
 
 #define DHTPIN 4
-#define DHTRESPONCE 2000
+#define DHTRESPONCE 6000
 #define DHTTYPE DHT22
 
 #define SDCSPIN 10
@@ -20,6 +20,8 @@
 #define HM10RX 3
 
 #define TIME_BETWEEN_SAVES 6000  // 1 MIN
+
+#define ACK_TIMEOUT_TIME 5000
 
 struct DHTData {
   float tempreature;
@@ -44,6 +46,7 @@ unsigned long DHTData::lastSaved = millis();
 struct Modules {
   DHT *dht;
   bool isSDinitialized;
+  bool isBLEConnected;
   virtuabotixRTC myRTC = virtuabotixRTC(RTCCLK, RTCDAT, RTCRST);
   SoftwareSerial BTserial=SoftwareSerial(HM10TX, HM10RX);
   Modules() {
@@ -51,6 +54,7 @@ struct Modules {
     dht->begin();
     myRTC.updateTime();
     isSDinitialized = SD.begin(SDCSPIN);
+    isBLEConnected=0;
     BTserial.begin(9600);
   }
 
@@ -127,8 +131,11 @@ struct Modules {
       uint8_t file_day = name.toInt();
       if (file_day < from_day || file_day > to_day) continue;
       float avg = getDayAvg(file);
+      Serial.println(avg);
+      sendToBLE('\0',avg,false);//continue in mobile to check this code
     }
     dir.close();
+    sendToBLE('h',-1);
   }
 
   String readFromBLE(){
@@ -140,13 +147,74 @@ struct Modules {
     if(BTserial.available()){
       String data = BTserial.readString();
       if (data=="1"){
+        Serial.println("ACK Rec");
+        return true;
+      }
+      else if(data.startsWith("disconnect")){
+        isBLEConnected=0;
+        return true;
+      }
+      else if(data.startsWith("connected")){
+        isBLEConnected=true;
         return true;
       }
     }
     return false;
   }
-  bool sendToBLE(char type,float data){
-    BTserial.println(String(type)+String(data));
+  bool sendToBLE(char type,float data,bool flush=true){
+    static String buffer="";
+    flush = flush || buffer.length()>10;
+    String newData= ( (type=='\0') ? "":String(type))+String(data)+ ( (flush) ? "":",");
+    buffer+=newData;
+    if(!flush) return 1;
+    Serial.println(buffer);
+    BTserial.print(buffer);
+    buffer="";
+    long startWait=millis();
+    while(!isAckRecived())
+    {
+      if(millis()-startWait>ACK_TIMEOUT_TIME){// time out waiting for ack
+        isBLEConnected=0;
+        Serial.println("time out");
+        return 0;
+      }
+    }
+    return 1;
+  }
+  void checkBLECommands(){
+    String dataOnBLE=readFromBLE();
+    if(dataOnBLE.length()<2) return;
+    Serial.println(dataOnBLE);
+    if(dataOnBLE.startsWith("connected")){
+      isBLEConnected=1;
+      return; 
+    }
+    if(dataOnBLE.startsWith("disconnect")){
+      isBLEConnected=0;
+      return; 
+    }
+    if(dataOnBLE[0]=='h'){
+      if (dataOnBLE[1]=='d') {
+        this->myRTC.updateTime();
+        uint8_t from_day,to_day,current_day=myRTC.dayofmonth,current_month=myRTC.month;
+        int current_year=myRTC.year;
+        if(current_day<12){
+          retrieveDataDays(dataOnBLE[2],current_year,current_month,0,current_day);
+          current_month = (current_month==1) ? 12 : current_month-1;
+          if(current_month==1){
+            current_month=12;
+            current_year-=1;
+          }else{
+            current_month-=1;
+          }
+          uint8_t days_in_current_month=days_in_month(current_year,current_month);
+          retrieveDataDays(dataOnBLE[2],current_year,current_month,days_in_current_month-12,days_in_current_month);
+        }
+        else
+          retrieveDataDays(dataOnBLE[2],current_year,current_month,current_day-12,current_day);
+      }
+    }
+
   }
 };
 Modules *modules;
@@ -158,15 +226,18 @@ void setup() {
 void loop() {
   DHTData dht;
   String data;
-  if (modules->readDHT(dht)) {
+  if (modules->readDHT(dht)) { // TODO: if the data is equal to the last read data dont enter
     if (DHTData::isTimeToSave()) {
       modules->writeToSD(HUMIDITY, dht.humidity);
       modules->writeToSD(TEMPREATURE, dht.tempreature);
       DHTData::lastSaved = millis();
     }
-    modules->sendToBLE(TEMPREATURE,dht.tempreature);
-    delay(1000);
-    modules->sendToBLE(HUMIDITY,dht.humidity);
+    if(modules->isBLEConnected){
+      modules->sendToBLE(TEMPREATURE,dht.tempreature);
+      modules->sendToBLE(HUMIDITY,dht.humidity);
+    }
+
     
   }
+  modules->checkBLECommands();
 }
